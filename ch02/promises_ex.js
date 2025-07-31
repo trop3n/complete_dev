65ec07be2dcdcc0ddee873d055c386f69ffca36a
trop3n <jasondkimm@proton.me>
2025-07-31 16:13:54 -0500

    example of Promises and asynch in JS

--- ch02/promises_ex.js
+++ ch02/promises_ex.js
@@ -1,4 +1,9 @@
 function fetchData(url) {
     fetch(url)
         .then((response) => response.json())
+        .then((json) => console.log(json))
+        .catch((error) => {
+            console.error(`Error : ${error}`);
+        });
 }
+fetchData("https://www.usemodernfullstack.dev/api/v1/users");
