725d0923596414c15d059573a64e5d7c8bd8bdc7
trop3n <jasondkimm@proton.me>
2025-09-02 00:00:45 -0500

    updated index.js

--- sample-express/index.js
+++ sample-express/index.js
@@ -1,11 +1,22 @@
-const express = require('express');
-const server = express();
+import { routeHello, routeAPINames } from "./routes.js";
+import express from express();
 const port = 3000;
 
-server.get('/hello', function (req, res) {
-    res.send('Hello, World!');
+server.get("/hello", function (req, res) {
+    const response = routeHello(req, res);
+    res.send(response);
+});
+
+server.get("/api/names", async function (req, res) {
+    let response;
+    try {
+        response = await routeAPINames(req, res);
+    } catch (err) {
+        console.log(err);
+    }
+    res.send(response);
 });
 
 server.listen(port, function () {
-    console.log('Listening on ' + port);
+    console.log("Listening on " + port);
 });
