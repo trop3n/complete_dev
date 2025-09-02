3dc6f45daba51bd70af50a5cea71ea687eaf2141
trop3n <jasondkimm@proton.me>
2025-09-02 12:53:43 -0500

    implicit conversion of a number to a string in JavaScript

---
+++ ch03_typescript/conversion.js
@@ -0,0 +1,9 @@
+let string = "1";
+let number = 1;
+let result;
+
+result = number + number;
+console.log("value: ", result, " type of ", typeof(result));
+
+result = number + string;
+console.log("value: ", result, " type of ", typeof(result));
