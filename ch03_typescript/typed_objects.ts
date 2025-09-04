8d18d228359e3519c66c6945a213b4a82d91704a
trop3n <jasondkimm@proton.me>
2025-09-04 09:19:58 -0500

    typed objects in typescript

---
+++ ch03_typescript/typed_objects.ts
@@ -0,0 +1,6 @@
+let weatherDetail: {
+    weather: string,
+    zipcode: string,
+    temp: number
+} = { weather: "sunny", zipcode: "00000", temp: 1 };
+weatherDetail.weather = 2;
---
+++ ch03_typescript/union_type.ts
@@ -0,0 +1,4 @@
+let stringOrNumberUnionType: string | number;
+stringOrNumberUnionType = "bar";
+stringOrNumberUnionType = 1;
+stringOrNumberUnionType = true;
