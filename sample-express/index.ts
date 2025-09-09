aa6111e86b7c6c668b4da1be78f221f60c52a0d5
trop3n <jasondkimm@proton.me>
2025-09-08 23:20:59 -0500

    updated index.ts

--- sample-express/index.ts
+++ sample-express/index.ts
@@ -1,23 +1,33 @@
-import { routeHello, routeAPINames } from "./routes.js";
-import express from "express";
+import { routeHello, routeAPINames, routeWeather } from "./routes.js";
+import express, { Request,  Response } from "express";
 
 const server = express();
 const port = 3000;
 
-server.get("/hello", function (req, res) {
-    const response = routeHello(req, res);
+server.get("/hello", function (_req: Request, res: Response): void {
+    const response = routeHello();
     res.send(response);
 });
 
-server.get("/api/names", async function (req, res) {
-    let response;
-    try {
-        response = await routeAPINames(req, res);
-    } catch (err) {
-        console.log(err);
+server.get("/api/names", 
+    async function (_req: Request, res: Response): Promise<void> {
+        let response;
+        try {
+            response = await routeAPINames();
+            res.send(response);
+        } catch (err) {
+            console.log(err);
+        }
     }
-    res.send(response);
-});
+);
+
+server.get(
+    "/api/weather/:zipcode",
+    function (req: Request, res: Response): void {
+        const response = routeWeather({ zipcode: req.params.zipcode });
+        res.send(response);
+    }
+);
 
 server.listen(port, function () {
     console.log("Listening on " + port);
